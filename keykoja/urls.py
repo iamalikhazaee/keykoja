7bca2f4cd389e614fbb0485efaa7d74cf9659c6c
alikhazaei <alikhazaee00@gmail.com>
2023-04-10 10:31:43 +0330

    core path added to the urls

--- keykoja/urls.py
+++ keykoja/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from core import url as app_url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('core/', include(app_url)),
 ]
