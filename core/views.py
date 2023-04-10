b92bf8ee00da855683c19b07c4a9f40746f99449
alikhazaei <alikhazaee00@gmail.com>
2023-04-10 10:30:41 +0330

    core views.py added

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from .models import *
+from .serializer import *
 # Create your views here.
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
