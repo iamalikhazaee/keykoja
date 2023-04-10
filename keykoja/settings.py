addd4976684abc05a8e91e163836899590588844
alikhazaei <alikhazaee00@gmail.com>
2023-04-10 11:02:51 +0330

    debug toolbar added to the project (update)

--- keykoja/settings.py
+++ keykoja/settings.py
@@ -53,6 +53,14 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
+
+INTERNAL_IPS = [
+     # ...
+     "127.0.0.1",
+     # ...
+ ]
+
+
 ROOT_URLCONF = 'keykoja.urls'
 
 TEMPLATES = [
