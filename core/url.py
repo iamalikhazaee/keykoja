3538c01a752ba635678ebbf0f7a5987072cbc408
alikhazaei <alikhazaee00@gmail.com>
2023-04-10 10:30:07 +0330

    core url.py added

--- core/url.py
+++ core/url.py
@@ -1,4 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import *
 
+
+router = DefaultRouter()
+router.register(r'Profiles', ProfileViewSet, basename='Profiles')
 
 urlpatterns = [
     path('', include(router.urls))
