08b45d8ab310a13aedb1c4474fa9302a45c584ad
alikhazaei <alikhazaee00@gmail.com>
2023-04-13 18:04:37 +0330

    admin.py updated because od availability model

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from core import models
 
 admin.site.register(models.Profile)
-admin.site.register(models.Time)
+admin.site.register(models.Availability)
 # Register your models here.
