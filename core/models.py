cb28f84cef3044cc8c099cf848c2f54bf6d2040d
alikhazaei <alikhazaee00@gmail.com>
2023-04-13 18:05:17 +0330

    Availabilty times added to the model

--- core/models.py
+++ core/models.py
@@ -2,27 +2,40 @@ from django.db import models
 
 # Create your models here.
 
-class Time(models.Model):
-    day = models.DateField()
-    hour = models.DateTimeField()
-    def __str__(self):
-        self.day + self.hour
+class Availability(models.Model):
+    DAYS_OF_WEEK = (
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
+        ('Saturday', 'Saturday'),
+        ('Sunday', 'Sunday')
+    )
+    day_of_week = models.CharField(max_length=255 ,choices=DAYS_OF_WEEK)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
 
     class Meta:
         db_table = ''
         managed = True
-        verbose_name = 'Time'
-        verbose_name_plural = 'Times'
+        verbose_name = 'Availability'
+        verbose_name_plural = 'Availabilities'
 
+    def __str__(self):
+        return  f'{self.day_of_week}' + f'{str(self.start_time)}'
+    
 class Profile(models.Model):
+    email = models.EmailField(null=False, blank=False)
+    password = models.CharField(max_length=255)
 
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.EmailField(null=False, blank=False)
-    phone = models.CharField(max_length=50)
-    time = models.ForeignKey(Time, on_delete=models.CASCADE)
+    domain = models.CharField(max_length=50)
+    
+    availability = models.ManyToManyField(Availability)
     def __str__(self):
-        pass
+        self.first_name + self.last_name
 
     class Meta:
         db_table = ''
@@ -46,7 +59,7 @@ class Event(models.Model):
     )
     place = models.CharField(max_length=1, choices=MY_CHOICES_place)
     massage = models.TextField(blank=True)
-    default_time = models.ForeignKey(Time, on_delete=models.CASCADE)
+    default_time = models.ForeignKey(Availability, on_delete=models.CASCADE)
 
 
     def __str__(self):
