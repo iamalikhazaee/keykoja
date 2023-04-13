9e6d3335dee16c88b473d8eaf8757b4398c8558c
alikhazaei <alikhazaee00@gmail.com>
2023-04-13 17:44:58 +0330

    Changed databse to Postgresql

--- keykoja/settings.py
+++ keykoja/settings.py
@@ -85,13 +85,23 @@ WSGI_APPLICATION = 'keykoja.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'keykoja',
+        'USER': 'alikhazaei',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
