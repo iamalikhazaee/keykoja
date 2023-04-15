15d357cb5ab1383910c36fd79764b6efde42a096
alikhazaei <alikhazaee00@gmail.com>
2023-04-15 18:10:45 +0330

    guest url added

--- core/url.py
+++ core/url.py
@@ -6,6 +6,7 @@ from .views import *
 router = DefaultRouter()
 router.register(r'Profiles', ProfileViewSet, basename='Profiles')
 router.register(r'NewEvent', NewEventViewSet, basename='New_Event')
+router.register(r'Guests', GuestViewSet, basename='Guests')
 
 urlpatterns = [
     path('', include(router.urls))
