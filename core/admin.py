24ab676a8c103adcf03e0eb8d9bc191e944e5edf
alikhazaei <alikhazaee00@gmail.com>
2023-04-15 15:42:11 +0330

    guest admin added

--- core/admin.py
+++ core/admin.py
@@ -4,4 +4,5 @@ from core import models
 admin.site.register(models.Profile)
 admin.site.register(models.Availability)
 admin.site.register(models.Event)
+admin.site.register(models.Guest)
 # Register your models here.
