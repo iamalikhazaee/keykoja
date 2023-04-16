88015047ae1af2ad1f7fe61666b968ba726b9a88
alikhazaei <alikhazaee00@gmail.com>
2023-04-16 14:50:52 +0330

    get profile's guests list from events , approve thr guest request

--- core/views.py
+++ core/views.py
@@ -13,23 +13,32 @@ class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
+    @action(detail=True, methods=['get'])
+    def events(self, request, pk=None):
+        profile = self.get_object()
+        events = Event.objects.filter(owner=profile)
+        serializer = NewEventSerializer(events, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=True, methods=['get'])
+    def guests(self, request, pk=None):
+        profile = self.get_object()
+        guests = Guest.objects.filter(event__owner=profile)
+        serializer = GuestSerializer(guests, many=True)
+        return Response(serializer.data)
+    
 class GuestViewSet(viewsets.ModelViewSet):
     queryset = Guest.objects.all()
     serializer_class = GuestSerializer
 
     @action(detail=True, methods=['Get','put'])
-    def deactive_guest(self, request, pk=None):
+    def approve(self, request, pk=None):
         Guest = self.get_object()
-        Guest.active = True
+        Guest.approve = True
         Guest.save()
         serializer = self.get_serializer(Guest)
         return Response(serializer.data)
     
-    @action(detail=True, methods=['delete','Get'])
-    def delete_guest(self, request, pk=None):
-        Guest = self.get_object()
-        Guest.delete()
-        return Response({'Message': 'Object deleted successfully.'},status=status.HTTP_204_NO_CONTENT)
 
 class NewEventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
