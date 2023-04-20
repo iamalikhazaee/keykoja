28110ccd5e25149fe14ff5e89696578a3d78a715
alikhazaei <alikhazaee00@gmail.com>
2023-04-20 23:34:24 +0330

    availability route added

--- core/url.py
+++ core/url.py
@@ -7,6 +7,7 @@ router = DefaultRouter()
 router.register(r'Profiles', ProfileViewSet, basename='Profiles')
 router.register(r'NewEvent', NewEventViewSet, basename='New_Event')
 router.register(r'Guests', GuestViewSet, basename='Guests')
+router.register(r'Availability', AvailabilityViewset, basename='Availabilities')
 
 urlpatterns = [
     path('', include(router.urls))
