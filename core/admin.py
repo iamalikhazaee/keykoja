44a7d685f435697475bd75104f10a588092651ce
alikhazaei <alikhazaee00@gmail.com>
2023-04-20 23:37:44 +0330

    class personAdmin added

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from core import models
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name',)
 
-admin.site.register(models.Profile)
+admin.site.register(models.Profile,PersonAdmin)
 admin.site.register(models.Availability)
 admin.site.register(models.Event)
 admin.site.register(models.Guest)
