09be2bd286b18a1a3e97b2bf720906052f8dfcf8
alikhazaei <alikhazaee00@gmail.com>
2023-05-19 23:11:04 +0330

    user serializer added

--- backend/core/serializer.py
+++ backend/core/serializer.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import *
 
-
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','password','email']
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
