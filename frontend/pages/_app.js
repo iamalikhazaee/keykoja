6450facc023ed6b5a01fffd28480b9936409b9ab
Fateme Hanifi <fatemehhanifi1379@gmail.com>
2023-05-19 15:32:04 +0330

    bug fix and edited add event modal.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import '@/styles/globals.scss'
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import 'bootstrap/dist/css/bootstrap.css';
 config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
