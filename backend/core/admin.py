f84b784bf45d82ef7eaea624c6c37b10f1670710
alikhazaei <alikhazaee00@gmail.com>
2023-06-23 16:20:12 +0330

    admin.py is cleaned

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
 from core import models
-class PersonAdmin(admin.ModelAdmin):
-    list_display = ('first_name',)
 
-admin.site.register(models.ProfileUser,PersonAdmin)
-admin.site.register(models.Availability)
+admin.site.register(models.ProfileUser)
 admin.site.register(models.Event)
 admin.site.register(models.Guest)
 admin.site.register(models.Event_time)
